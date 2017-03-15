cbc18db2cd07bd4c1a099c864007abeb069c3080
William Weckl <william.weckl@gmail.com>
2017-03-15 20:29:08 -0300

    skiping shuffle test doc

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -1,8 +1,14 @@
 defmodule CardsTest do
   use ExUnit.Case
-  doctest Cards
+  doctest Cards, except: [deal: 2]
 
-  # test "" do
-  #
-  # end
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 20
+  end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+    refute deck == Cards.shuffle(deck)
+  end
 end
